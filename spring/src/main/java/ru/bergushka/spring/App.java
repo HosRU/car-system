1cf29fd1d4cec4a0a02fbb238ea1fca3b22d80d5
Artemounchik <100020729+HosRU@users.noreply.github.com>
2026-03-31 11:28:35 +0300

    Rework

--- spring/src/main/java/ru/bergushka/spring/App.java
+++ spring/src/main/java/ru/bergushka/spring/App.java
@@ -1,13 +1,10 @@
 package ru.bergushka.spring;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();    
     }
 }
